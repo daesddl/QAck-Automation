c43335d00a446ac332087a319a3ec268418249bb
Daniel Espinosa <despinos@starbucks.com>
2024-04-07 12:57:36 -0600

    Adding Conditional Loop Homework

---
+++ Homework/Conditional Loops/conditional_loops.js
@@ -0,0 +1,39 @@
+// Ejercicio 1: Usa un bucle "if else" para determinar si un número es par o impar.
+
+// Ejercicio 2: Crea un programa de calculadora básico que acepte dos números y una operación (+, -, *, /) y usa "if else" para procesar la operación seleccionada.
+
+// Ejercicio 3: Solicita al usuario un número y luego usa "if else" para determinar si el número es positivo, negativo o cero.
+
+// Ejercicio 4: Haz un programa que compare dos números dados. Si son iguales, debe mostrar un mensaje de "son iguales", en caso contrario, "son diferentes".
+
+// Ejercicio 5: Usa un bucle "if else" para determinar si un número es primo o no.
+
+// Ejercicio 6: Escribe un programa que solicite a un usuario su edad e imprima si es mayor de edad o no.
+
+// Ejercicio 7: Escribe un programa que requiera a los usuarios que califiquen un producto en una escala de 1 a 5 e imprima diferentes respuestas según la calificación.
+
+// Ejercicio 8: Escribe un programa que solicite a un usuario su índice de masa corporal (IMC) y determine su estado nutricional.
+
+// Ejercicio 9: Escribe un programa que le pida a un usuario que adivine un número y le permita seguir adivinando hasta que acierte el número.
+
+// Ejercicio 10: Escribe un programa que solicite al usuario un valor en grados Celsius y conviértalo a Fahrenheit usando un "if else".
+
+// Ejercicio 11: Escribe un programa que solicite tres números e imprima el mayor de ellos.
+
+// Ejercicio 12: Hacer un programa que te solicite una contraseña y sólo te permita tres intentos para acertarla.
+
+// Ejercicio 13: Escribe un código donde dada una calificación, muestres si es reprobatoria, aprobatoria, destacada o sobresaliente (puedes definir el rango).
+
+// Ejercicio 14: Crea un programa que solicite al usuario un día de la semana e imprima si es un día laboral o fin de semana.
+
+// Ejercicio 15: Crea un código que solicite una letra al usuario e indique si es una vocal o una consonante.
+
+// Ejercicio 16: Escribe un programa que solicite la hora actual e imprima buenos días, buenas tardes o buenas noches según corresponda.
+
+// Ejercicio 17: Escribe un código que le pida al usuario su año de nacimiento e imprima su signo zodiacal.
+
+// Ejercicio 18: Condiciona una frase en que dependiendo de la hora del día, el saludo sea diferente.
+
+// Ejercicio 19: Escribe un programa donde tengas una lista de usuarios y te permita buscar si un usuario está en esa lista.
+
+// Ejercicio 20: Crea un simple juego de pregunta respuesta usando "if else". Como un cuestionario simple de verdadero o falso.
